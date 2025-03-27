79444c0d369dc603ac56a5d687339d610b5d7f1c
JananiUpeksha <upekshakeshani@gmail.com>
2025-03-27 16:36:35 +0530

    Project and DB setup

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,21 @@
+import express from "express"
+import {signup,login,logout} from "../controllers/auth.controller.js"
+
+
+
+
+const router = express.Router();
+
+/*
+router.post("/signup", (req, res) => {
+    res.send("signup route");
+});
+*/
+
+router.post("/signup",signup);
+
+router.post("/login",login);
+
+router.post("/logout",logout);
+
+export default router;
